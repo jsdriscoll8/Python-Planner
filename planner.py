603e698f6250a5d7647e4f6c82a11e7a9fa947b0
jsdriscoll8 <jsdriscoll8@gmail.com>
2024-01-18 17:06:50 -0500

    added listeners

--- planner.py
+++ planner.py
@@ -1,25 +1,33 @@
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 import sys
 
 class PyPlannerWindow(QMainWindow):
     def __init__(self):
         # Initialize, set title, set size
         super().__init__()
+        self.buttonChecked = False
         self.setWindowTitle("PyPlanner")
         self.setFixedSize(QSize(1000, 500))
 
         # Draw UI components
         self.uiComponents()
 
+
     def uiComponents(self):
-        # Create task button in top left corner
+        # Create task button in top left corner, listening for click
         button = QPushButton("Create task", self)
         button.setGeometry(0, 0, 333, 125)
+        button.setCheckable(True)
 
+        button.clicked.connect(self.createTaskToggled)
+
+    def createTaskToggled(self):
+        self.buttonChecked = True
+        print(self.buttonChecked)
 
 app = QApplication(sys.argv)
-
 window = PyPlannerWindow()
 window.show()
 
